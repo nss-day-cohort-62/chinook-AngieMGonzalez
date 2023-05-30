24dcd9d0ec305efdf170e283d60618be660da6c1
Angie Gonzalez <angiem.go@gmail.com>
2023-05-30 14:48:20 -0400

    e17 is the same as e11

---
+++ invoices_line_item_count.sql
@@ -0,0 +1,12 @@
+--E17 invoices_line_item_count.sql:
+-- Provide a query that shows all Invoices. The resultant table should include:
+-- InvoiceId
+-- The total number of line items on each invoice
+
+SELECT 
+  il.InvoiceId, 
+  COUNT(il.InvoiceLineId) AS lineItems
+FROM InvoiceLine il
+JOIN invoice i 
+  ON il.InvoiceId = i.InvoiceId
+GROUP BY il.InvoiceId;
--- line_items_per_invoice.sql
+++ line_items_per_invoice.sql
@@ -1,4 +1,7 @@
+--E11
+-- line_items_per_invoice.sql:
 -- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice.
+-- this is the same as e17
 
 SELECT InvoiceId, COUNT(*) AS line_items
 FROM InvoiceLine
